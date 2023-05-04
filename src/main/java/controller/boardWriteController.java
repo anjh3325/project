c6c88a71b45b77d11bf387e4d9ece8aab66e4596
luffy <luffy@gmail.com>
2023-05-04 09:35:44 +0900

    fix:기능보완

--- src/main/java/controller/boardWriteController.java
+++ src/main/java/controller/boardWriteController.java
@@ -15,7 +15,7 @@ public class boardWriteController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		HttpSession session = req.getSession();
-	
+		System.out.println("!");
 		String continent = req.getParameter("continent"); //대륙값
 		String country = req.getParameter("country");
 		req.getRequestDispatcher("/WEB-INF/views/moim/boardWrite?continent=" + continent + "&country=" + country).forward(req, resp); 
